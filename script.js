51b94873c5fb465e3ba021896ab561b96b4c6a06
nehapansare <nehapansare007@gmail.com>
2024-07-12 10:20:09 +0530

    sum of add number code added

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ for(let i=0;i<=value.length;i++)
     }
 }
 console.log(largest)
+
+const num=[2,7,13,4,3,9,16]
+let sum=0;
+for(let i=0;i<num.length;i++)
+{
+    if(num[i]%2!==0)
+    {
+            sum=sum+num[i];
+    }
+}
+console.log(`sum of odd number is:${sum}`)
